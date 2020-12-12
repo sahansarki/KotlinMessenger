5c0bdbca80ab723a53955059d19477e11bb14cfe
sahan <shnsrk123@gmail.com>
2020-12-13 01:09:54 +0300

    Third Push -- Register and Login (with firebase)

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,13 +1,35 @@
 package com.example.kotlinmessenger
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     setContentView(R.layout.activity_login)
+    val login_button = findViewById<Button>(R.id.login_login_button)
+    val back_to_register_textview = findViewById<TextView>(R.id.back_register_textview)
+
+    login_button.setOnClickListener {
+        val email = findViewById<TextView>(R.id.login_email).text.toString()
+        val password = findViewById<TextView>(R.id.login_password).text.toString()
+
+        Log.d("Login" , "Attempt login with email/password: ${email}/***}")
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+//            .addOnCompleteListener()
+//            .add
+
+    }
+
+    back_to_register_textview.setOnClickListener {
+        finish()
+    }
     }
 }
--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +17,7 @@ class MainActivity : AppCompatActivity() {
         val alread_have_account = findViewById<TextView>(R.id.alread_have_account_textView)
         val register_button = findViewById<Button>(R.id.register_button_register)
         register_button.setOnClickListener {
-            val email = findViewById<TextView>(R.id.email_edittext_register)
-            val password = findViewById<TextView>(R.id.password_edittext_register)
-
-            Log.d("MainActivity" , " Email is " + email.text)
-            Log.d("MainActivity" , " Password is : ${password.text}")
-
+            performRegister()
         }
         alread_have_account.setOnClickListener {
             Log.d("MainActivity" , "Try to show login activity")
@@ -28,6 +25,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
+
+    }
+    private fun performRegister(){
+        val email = findViewById<TextView>(R.id.email_edittext_register).text.toString()
+        val password = findViewById<TextView>(R.id.password_edittext_register).text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/password",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity" , " Email is $email" )
+        Log.d("MainActivity" , " Password is : ${password}")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    // else if succesfully
+                    Log.d("Main", "Succesfully created user with uid : ${it.result!!.user!!.uid}")
+                }
+                .addOnFailureListener {
+                    Log.d("Main" , "Fail to create user: ${it.message}")
+                    Toast.makeText(this,"Fail to create user: ${it.message}",Toast.LENGTH_SHORT).show()
+                }
+
 
     }
 
